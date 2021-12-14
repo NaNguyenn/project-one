57a8c2f5c4f62b171dda43b44b02186421115991
NguyennNguyenn <yu19151998@gmail.com>
2021-12-14 21:51:10 +0700

    first commit

---
+++ src/Categories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+Categories.propTypes = {
+
+};
+
+function Categories(props) {
+    return (
+        <div>
+            <h1>Categories component</h1>
+        </div>
+    );
+}
+
+export default Categories;
---
+++ src/Listing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+Listing.propTypes = {
+
+};
+
+function Listing(props) {
+    return (
+        <div>
+            <h1>Listing component</h1>
+        </div>
+    );
+}
+
+export default Listing;
